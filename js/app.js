228cb69dd13aaf5e8b0f33ccc73c4a87bd07f488
Lorran David <lorran@esgn.com.br>
2018-01-18 13:07:34 -0200

    ES6

--- js/app.js
+++ js/app.js
@@ -1,71 +1,42 @@
-(function(document, $) {
+(function IIFE(document, $) {
   'use strict';
 
-  /*********************************************/
-  var Helpers = {
-    isRequestSuccessful:  function isRequestSuccessful (request) {
+  /** ****************************************** */
+  const Helpers = {
+    isRequestSuccessful: function checkIfRequestIsSuccessful(request) {
       return request.readyState === 4 && request.status === 200;
     },
 
-    isPropImg: function isPropImg (string) {
+    isPropImg: function checkIfStringEqualsImage(string) {
       return string === 'image';
     },
 
-    validateFormEntries: function (obj) {
-      for (let prop in obj) {
-        if (obj[prop] === '') return false
-      }
-
-      return true;
+    validateFormEntries: function checkIfHasEmptyValue(obj) {
+      const values = Object.keys(obj).map(key => obj[key]);
+      return values.includes('');
     },
 
-    isTagEqual: function (string, stringCompare) {
+    isTagEqual: function checkIfStringsAreEqual(string, stringCompare) {
       return string.toLowerCase() === stringCompare.toLowerCase();
-    }
+    },
   };
 
-  /*********************************************/
-  var setupUI = function () {
+  /** ****************************************** */
+  const setupUI = function UIFactory() {
     const $btnRemoveTemplate = '<a href="#" class="button button-accent" data-js="btnRemove">Remover</a>';
-    var $title = $('title');
-    var $companyName = $('[data-js="companyName"]');
-    var $companyDescription = $('[data-js="companyDescription"]');
-    var $companyPhone = $('[data-js="companyPhone"]');
-    var $formRegisterCar = $('[data-js="formRegisterCar"]');
-    var $tableRecords = $('[data-js="recordsTable"]');
-    var $imgInput = $('[data-js="imgInput"]');
-    var $modeloInput = $('[data-js="modeloInput"]');
-    var $anoInput = $('[data-js="anoInput"]');
-    var $placaInput = $('[data-js="placaInput"]');
-    var $corInput = $('[data-js="corInput"]');
+    const $title = $('title');
+    const $companyName = $('[data-js="companyName"]');
+    const $companyDescription = $('[data-js="companyDescription"]');
+    const $companyPhone = $('[data-js="companyPhone"]');
+    const $formRegisterCar = $('[data-js="formRegisterCar"]');
+    const $tableRecords = $('[data-js="recordsTable"]');
+    const $imgInput = $('[data-js="imgInput"]');
+    const $modeloInput = $('[data-js="modeloInput"]');
+    const $anoInput = $('[data-js="anoInput"]');
+    const $placaInput = $('[data-js="placaInput"]');
+    const $corInput = $('[data-js="corInput"]');
 
-    var init = function () {
-      initEvents();
-    };
-
-    var initEvents = function () {
-      $formRegisterCar.on('submit', handleSubmitForm, false);
-    };
-
-    var handleSubmitForm = function (e) {
-      e.preventDefault();
-
-      var data = {
-        img: $imgInput.get().value,
-        modelo: $modeloInput.get().value,
-        ano: $anoInput.get().value,
-        placa: $placaInput.get().value,
-        cor: $corInput.get().value
-      };
-
-      if(!Helpers.validateFormEntries(data))
-        return;
-
-      clearForm();
-      App.addCar(data);
-    };
-
-    var clearForm = function () {
+    const clearForm = function clearForm() {
       $imgInput.get().value = '';
       $modeloInput.get().value = '';
       $anoInput.get().value = '';
@@ -73,71 +44,93 @@
       $corInput.get().value = '';
     };
 
-    var fillCompanyInfo = function (data) {
+    const handleSubmitForm = function handleSubmitForm(e) {
+      e.preventDefault();
+
+      const data = {
+        img: $imgInput.get().value,
+        modelo: $modeloInput.get().value,
+        ano: $anoInput.get().value,
+        placa: $placaInput.get().value,
+        cor: $corInput.get().value,
+      };
+
+      if (!Helpers.validateFormEntries(data)) {
+        return;
+      }
+
+      clearForm();
+      App.addCar(data);
+    };
+
+    const initEvents = function initUIEvents() {
+      $formRegisterCar.on('submit', handleSubmitForm, false);
+    };
+
+    const init = function initUI() {
+      initEvents();
+    };
+
+    const fillCompanyInfo = function fillCompanyInfo(data) {
       $title.get().textContent = data.title;
       $companyDescription.get().textContent = data.description;
       $companyPhone.get().textContent = data.phone;
 
-      $companyName.forEach(function(item) {
-        item.textContent = data.name;
+      $companyName.forEach((item) => {
+        const $nameEl = item;
+        $nameEl.textContent = data.name;
       });
     };
 
-    var addCarToTable = function (data) {
-      var $docFragment = document.createDocumentFragment();
-      var $newRow = document.createElement('tr');
+    const handleRemoveCarFromTable = function handleRemoveCarFromTable(e) {
+      e.preventDefault();
 
-      for(let prop in data) {
-        var $newColumn = document.createElement('td');
-        if(Helpers.isPropImg(prop)) {
-          {
-            let $tdImg = document.createElement('img');
-            $tdImg.setAttribute('src', data[prop]);
-            $newColumn.appendChild($tdImg);
-          }
+      if (!Helpers.isTagEqual(e.target.tagName, 'a')) return;
+      App.removeCar(this.getAttribute('data-js'));
+    };
+
+    const addCarToTable = function addCarToTable(data) {
+      const $docFragment = document.createDocumentFragment();
+      const $newRow = document.createElement('tr');
+      const $btnRemoveColumn = document.createElement('td');
+
+      const keys = Object.keys(data);
+      keys.forEach((prop) => {
+        const $newColumn = document.createElement('td');
+        if (Helpers.isPropImg(prop)) {
+          const $tdImg = document.createElement('img');
+          $tdImg.setAttribute('src', data[prop]);
+          $newColumn.appendChild($tdImg);
         } else {
           $newColumn.textContent = data[prop];
         }
         $newRow.appendChild($newColumn);
-      }
+      });
 
-      {
-        let $btnRemoveColumn = document.createElement('td');
-        $btnRemoveColumn.setAttribute('data-js', data.plate);
-        $btnRemoveColumn.innerHTML = $btnRemoveTemplate;
-        $btnRemoveColumn.addEventListener('click', handleRemoveCarFromTable, false);
-        $newRow.appendChild($btnRemoveColumn);
-      }
-
+      $btnRemoveColumn.setAttribute('data-js', data.plate);
+      $btnRemoveColumn.innerHTML = $btnRemoveTemplate;
+      $btnRemoveColumn.addEventListener('click', handleRemoveCarFromTable, false);
+      $newRow.appendChild($btnRemoveColumn);
       $docFragment.appendChild($newRow);
       $tableRecords.get().children[1].appendChild($docFragment);
     };
 
-    var handleRemoveCarFromTable = function (e) {
-      e.preventDefault();
-
-      if(!Helpers.isTagEqual(e.target.tagName, 'a')) return;
-      App.removeCar(this.getAttribute('data-js'));
+    const clearRecordsTable = function clearRecordsTable() {
+      const $tableBody = $tableRecords.get().children[1];
+      while ($tableBody.firstChild) {
+        $tableBody.removeChild($tableBody.firstChild);
+      }
     };
 
-    var populateTable = function (arr) {
+    const populateTable = function populateTable(arr) {
       clearRecordsTable();
 
-      arr.forEach(function(car){
+      arr.forEach((car) => {
         addCarToTable(car);
       });
     };
 
-    var clearRecordsTable = function () {
-      {
-        let $tableBody = $tableRecords.get().children[1];
-        while($tableBody.firstChild) {
-          $tableBody.removeChild($tableBody.firstChild);
-        }
-      }
-    };
-
-    var publicAPI = {
+    const publicAPI = {
       init: init,
       addCarToTable: addCarToTable,
       fillCompanyInfo: fillCompanyInfo,
@@ -147,81 +140,76 @@
     return publicAPI;
   };
 
-  /*********************************************/
-  var setupApp = function (UI) {
-    var init = function () {
-      getCompanyInfo();
-      getCars();
-    };
-
-    var getCompanyInfo = function () {
-      var ajax = new XMLHttpRequest();
-      ajax.open('GET', 'js/company.json');
-      ajax.send();
-      ajax.addEventListener('readystatechange', handleAjaxCompanyInfo, true);
-    };
-
-    var handleAjaxCompanyInfo = function () {
-      if(!Helpers.isRequestSuccessful(this))
-        return;
+  /** ****************************************** */
+  const setupApp = function AppFactory(UI) {
+    const handleAjaxCompanyInfo = function handleAjaxCompanyInfo() {
+      if (!Helpers.isRequestSuccessful(this)) return;
 
       try {
-        {
-          let data = JSON.parse(this.response);
-          UI.fillCompanyInfo(data);
-        }
+        const data = JSON.parse(this.response);
+        UI.fillCompanyInfo(data);
       } catch (err) {
         throw new Error('Aconteceu um probleminha:' + err);
       }
     };
 
-    var addCar = function (data) {
-      var ajax = new XMLHttpRequest();
+    const getCompanyInfo = function getCompanyInfo() {
+      const ajax = new XMLHttpRequest();
+      ajax.open('GET', 'js/company.json');
+      ajax.send();
+      ajax.addEventListener('readystatechange', handleAjaxCompanyInfo, true);
+    };
+
+    const handleGetCars = function handleGetCars() {
+      if (!Helpers.isRequestSuccessful(this)) return;
+
+      try {
+        const data = JSON.parse(this.response);
+        UI.populateTable(data);
+      } catch (err) {
+        throw new Error('Aconteceu um probleminha:' + err);
+      }
+    };
+
+    const getCars = function getCars() {
+      const ajax = new XMLHttpRequest();
+      ajax.open('GET', 'http://localhost:3000/car');
+      ajax.send();
+      ajax.addEventListener('readystatechange', handleGetCars, true);
+    };
+
+    const handleAddCar = function handleAddCar() {
+      if (!Helpers.isRequestSuccessful(this)) return;
+      getCars();
+    };
+
+    const init = function initApp() {
+      getCompanyInfo();
+      getCars();
+    };
+
+    const handleRemoveCar = function handleRemoveCar(e) {
+      if (!Helpers.isRequestSuccessful(this)) return;
+      getCars();
+    };
+
+    const addCar = function addCar(data) {
+      const ajax = new XMLHttpRequest();
       ajax.open('POST', 'http://localhost:3000/car');
       ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
       ajax.send('image=' + data.img + '&brandModel=' + data.modelo + '&year=' + data.ano + '&plate=' + data.placa + '&color=' + data.cor);
       ajax.addEventListener('readystatechange', handleAddCar, true);
     };
 
-    var handleAddCar = function () {
-      if(!Helpers.isRequestSuccessful(this)) return;
-      getCars();
-    };
-
-    var getCars = function () {
-      var ajax = new XMLHttpRequest();
-      ajax.open('GET', 'http://localhost:3000/car');
-      ajax.send();
-      ajax.addEventListener('readystatechange', handleGetCars, true);
-    };
-
-    var handleGetCars = function () {
-      if(!Helpers.isRequestSuccessful(this)) return;
-
-      try {
-        {
-          let data = JSON.parse(this.response);
-          UI.populateTable(data);
-        }
-      } catch (err) {
-        throw new Error('Aconteceu um probleminha:' + err);
-      }
-    };
-
-    var removeCar = function (plate) {
-      var ajax = new XMLHttpRequest();
+    const removeCar = function removeCar(plate) {
+      const ajax = new XMLHttpRequest();
       ajax.open('DELETE', 'http://localhost:3000/car');
       ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
       ajax.send('plate=' + plate);
       ajax.addEventListener('readystatechange', handleRemoveCar, true);
     };
 
-    var handleRemoveCar = function (e) {
-      if(!Helpers.isRequestSuccessful(this)) return;
-      getCars();
-    };
-
-    var publicAPI = {
+    const publicAPI = {
       init: init,
       getCompanyInfo: getCompanyInfo,
       addCar: addCar,
@@ -239,4 +227,4 @@
   // Initialize App
   var App = setupApp(UI);
   App.init();
-})(document, window.DOM);
+}(document, window.DOM));
